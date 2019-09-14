717ecff848fd69d72d7539612a3327aec98f1934
mateo9822 <48372160+mateo9822@users.noreply.github.com>
2019-09-14 00:13:31 -0500

    Add files via upload

---
+++ Canario.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programainterfaz;
+
+/**
+ *
+ * @author Mateo
+ */
+public class Canario implements PuedeCantar { 
+    public void cantar() { 
+        System.out.println("pio pio pio"); 
+    } 
+}
+
